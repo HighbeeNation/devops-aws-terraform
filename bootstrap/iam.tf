bfa95c3b0eeffeef3db86970e517d00973a9de14
HighbeeNation <isanni007@yahoo.com>
2024-10-27 19:41:10 -0400

    add: user & state storage s3

---
+++ bootstrap/iam.tf
@@ -0,0 +1,74 @@
+resource "aws_iam_user" "terraform_user" {
+  name = var.terraform_username
+
+  tags = {
+    purpose = "User to deploy Resoruces via Terrafoarm"
+  }
+}
+
+
+# User access state file
+data "aws_iam_policy_document" "s3_policy_document" {
+
+  statement {
+    sid = "List any Bucket in our S3"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+    effect = "Allow"
+
+  }
+
+  statement {
+
+    sid = "User Access State file"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.state_bucket.arn}/terraform/.tfstate",
+    ]
+  }
+}
+
+
+# User Permissions
+data "aws_iam_policy_document" "user_permissions" {
+
+  statement {
+    sid = "List permissions this user has"
+
+    actions = var.user_permission_list
+
+    resources = [
+      "*",
+    ]
+    effect = "Allow"
+
+  }
+
+}
+
+# combine document policies into one 
+data "aws_iam_policy_document" "terraform_user_policy_document" {
+    override_policy_documents = [
+        data.aws_iam_policy_document.s3_policy_document.json,
+        data.aws_iam_policy_document.user_permissions.json
+    ]
+}
+
+# attach combined policy document to user 
+resource "aws_iam_user_policy" "terraform_policy" {
+  name = "terraform-policy"
+  user = aws_iam_user.terraform_user.arn
+  policy = data.aws_iam_policy_document.terraform_user_policy_document.json
+}
---
+++ bootstrap/variables.tf
@@ -0,0 +1,23 @@
+variable "bucket_name" {
+  type = string
+
+   validation {
+    condition = length(var.bucket_name) > 0
+    error_message = "terraform user name not set"
+  }
+}
+
+variable "terraform_username" {
+  type = string
+
+  validation {
+    condition = length(var.terraform_username) > 0
+    error_message = "terraform user name not set"
+  }
+}
+
+variable "user_permission_list" {
+  description = "List of permissions to create or access resources by our terraform user"
+  type =  list(string)
+  default = [ "s3:CreateBucket" ]
+}
